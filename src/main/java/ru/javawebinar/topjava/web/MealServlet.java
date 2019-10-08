d02c84e8afee4378cbbeedded98291d26b787540
Vova <vladimirusa1@gmail.com>
2019-10-08 17:06:05 +0300

    Create MealServlet and doGet. Made initialization meals in one place. Added getters and setters in MealTo.

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,35 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalTime;
+
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class MealServlet extends HttpServlet {
+    private static String LIST_MEAL = "/meals.jsp";
+    private static final Logger log = getLogger(MealServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        log.debug("redirect to meals");
+        String forward = "";
+        String action = request.getParameter("action");
+
+        if (action.equalsIgnoreCase("mealList")) {
+            forward = LIST_MEAL;
+            request.setAttribute("meals", MealsUtil.getFiltered(MealsUtil.getMeals(), LocalTime.MIN, LocalTime.MAX, MealsUtil.getDefaultCaloriesPerDay()));
+        }
+
+        request.getRequestDispatcher(forward).forward(request, response);
+        response.sendRedirect("meals.jsp");
+    }
+}
