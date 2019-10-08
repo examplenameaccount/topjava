080d85622ba3f8d5842277fcc1c8f423979d0456
Vova <vladimirusa1@gmail.com>
2019-10-08 18:26:32 +0300

    Added MealService and MealServiceImpl with implementation.

---
+++ src/main/java/ru/javawebinar/topjava/service/MealServiceImpl.java
@@ -0,0 +1,37 @@
+package ru.javawebinar.topjava.service;
+
+import ru.javawebinar.topjava.model.Meal;
+
+import java.util.List;
+
+public class MealServiceImpl implements MealService {
+    @Override
+    public List<Meal> allMeals() {
+        return null;
+    }
+
+    @Override
+    public void add(Meal meal) {
+
+    }
+
+    @Override
+    public void delete(Meal meal) {
+
+    }
+
+    @Override
+    public void deleteMealById(int mealId) {
+
+    }
+
+    @Override
+    public void edit(Meal meal) {
+
+    }
+
+    @Override
+    public Meal getById(int id) {
+        return null;
+    }
+}
