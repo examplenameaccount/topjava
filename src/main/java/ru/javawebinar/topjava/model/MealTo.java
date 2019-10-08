51cf236ef236fc1d0272e806a2f07c6e74019d56
Vova <vladimirusa1@gmail.com>
2019-10-08 17:14:48 +0300

    Added variable id with getters and setters in MealTo and Meal. Fixed design index.html

--- src/main/java/ru/javawebinar/topjava/model/MealTo.java
+++ src/main/java/ru/javawebinar/topjava/model/MealTo.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MealTo {
     private final LocalDateTime dateTime;
@@ -9,14 +10,6 @@ public class MealTo {
 
     private final int calories;
 
-    private final boolean excess;
-
-    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
-        this.dateTime = dateTime;
-        this.description = description;
-        this.calories = calories;
-        this.excess = excess;
-    }
 
     public LocalDateTime getDateTime() {
         return dateTime;
@@ -33,6 +26,35 @@ public class MealTo {
     public boolean isExcess() {
         return excess;
     }
+
+    //    private final Supplier<Boolean> excess;
+//    private final AtomicBoolean excess;
+    private final boolean excess;
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public MealTo(LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+
+    public MealTo(int id, LocalDateTime dateTime, String description, int calories, boolean excess) {
+        this.id = id;
+        this.dateTime = dateTime;
+        this.description = description;
+        this.calories = calories;
+        this.excess = excess;
+    }
+
+//    public Boolean getExcess() {
+//        return excess.get();
+//    }
 
     @Override
     public String toString() {
