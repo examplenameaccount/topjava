f8e8a86f58677be5031cd2d01fbba76507b58a3f
Vova <vladimirusa1@gmail.com>
2019-10-15 23:44:58 +0300

    Meal extends AbstractBaseEntity, MealTo перенес в пакет ru.javawebinar.topjava.to

--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.topjava.util;
 
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.MealTo;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
