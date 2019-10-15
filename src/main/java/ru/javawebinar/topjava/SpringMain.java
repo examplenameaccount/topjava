f0a11a341b083c3f310dbaff002bf0dfd13ceaef
Vova <vladimirusa1@gmail.com>
2019-10-15 23:56:53 +0300

    Добавил класс еды из Spring и Logger в SpringMain и протестировал MealController.

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,20 +1,34 @@
 package ru.javawebinar.topjava;
 
+import org.slf4j.Logger;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.web.UserServlet;
+import ru.javawebinar.topjava.web.meal.MealRestController;
 import ru.javawebinar.topjava.web.user.AdminRestController;
 
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Arrays;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class SpringMain {
+    private static final Logger log = getLogger(SpringMain.class);
+
     public static void main(String[] args) {
         // java 7 automatic resource management
         try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml")) {
             System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
             AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
             adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ROLE_ADMIN));
+            MealRestController mealRestController = appCtx.getBean(MealRestController.class);
+            mealRestController.create(new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500));
+            mealRestController.getAll().forEach(a -> log.info(mealRestController.get(1).toString()));
+            mealRestController.getAll().forEach(a -> log.info(mealRestController.getAll().toString()));
         }
     }
 }
