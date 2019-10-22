a096d2684ede02625aa092db39e56483591ead3a
Vova <vladimirusa1@gmail.com>
2019-10-22 23:00:52 +0300

    Add initialization and populate MEALS in script initDB, populateDB.

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,11 +1,17 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
+
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
-INSERT INTO users (name, email, password) VALUES
-  ('User', 'user@yandex.ru', 'password'),
-  ('Admin', 'admin@gmail.com', 'admin');
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
 
-INSERT INTO user_roles (role, user_id) VALUES
-  ('ROLE_USER', 100000),
-  ('ROLE_ADMIN', 100001);
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (id, user_id, datetime, description, calories)
+VALUES (100000, 100000, '2015-05-30   10:00:00-00', 'Завтрак', 500),
+       (100001, 100000, '2015-05-31   20:00:00-00', 'Ужин', 2000);
