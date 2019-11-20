f56b0e86e288011716ea0509536764dbbc801492
Vova <vladimirusa1@gmail.com>
2019-11-21 00:26:51 +0200

    Fixed profile in isNotJdbcProfile().

--- src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -14,6 +14,7 @@ import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.ActiveDbProfileResolver;
+import ru.javawebinar.topjava.Profiles;
 import ru.javawebinar.topjava.TimingRules;
 
 import java.util.Arrays;
@@ -52,6 +53,6 @@ abstract public class AbstractServiceTest {
     }
 
     public boolean isNotJdbcProfile() {
-        return Arrays.stream(environment.getActiveProfiles()).noneMatch(profile -> profile.equals("jdbc"));
+        return Arrays.stream(environment.getActiveProfiles()).noneMatch(profile -> profile.equals(Profiles.JDBC));
     }
 }
